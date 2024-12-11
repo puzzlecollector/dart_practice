16ce16140cac05d54085fd0bcb58bdd10329f738
Luke Kim <puzzlecollector1123581321@gmail.com>
2024-12-11 12:04:19 +0900

    Create gesture_detector.dart

---
+++ gesture_detector.dart
@@ -0,0 +1,39 @@
+// 머터리얼 디자인과 관련된 기능을 불로오는 코드
+// material.dart 파일을 불러와야
+// 플러터에서 기본 제공해주는 위젯들을 사용할 수 있습니다.
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: GestureDetector(
+            onTap: () {
+              print('on tap');
+            },
+            onDoubleTap: () {
+              print('on double tap');
+            },
+            onLongPress: () {
+              print('on long press');
+            },
+            // 제스처를 적용할 위젯
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.red,
+              ),
+              width: 100.0,
+              height: 100.0,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
